b1840932a03521f3cab8362ef58ea3f4173b66d5
Rune Nielsen <runeanielsen@live.dk>
2020-02-23 01:36:52 +0100

    Adds coverage depedency

--- open_ftth.py
+++ open_ftth.py
@@ -21,7 +21,7 @@
  *                                                                         *
  ***************************************************************************/
 """
-from PyQt5 import QtWidgets, QtGui, QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, Qt, QUrl 
 from qgis.PyQt.QtGui import QIcon, QColor
 from qgis.PyQt.QtWidgets import QAction
