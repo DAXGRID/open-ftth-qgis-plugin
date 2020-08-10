ba98486d344765b29b0d0dbf4376601ca8dbfc84
Rune Nielsen <runeanielsen@live.dk>
2020-08-10 12:38:16 +0200

    init minimal plugin

--- __init__.py
+++ __init__.py
@@ -1,9 +1,20 @@
-def classFactory(iface):
-    """Load OpenFtth class from file OpenFtth.
+from PyQt5.QtWidgets import QAction, QMessageBox
 
-    :param iface: A QGIS interface instance.
-    :type iface: QgsInterface
-    """
-    #
-    from .open_ftth import OpenFtth
-    return OpenFtth(iface)
+def classFactory(iface):
+    return MinimalPlugin(iface)
+
+class MinimalPlugin:
+    def __init__(self, iface):
+        self.iface = iface
+
+    def initGui(self):
+        self.action = QAction('Go!', self.iface.mainWindow())
+        self.action.triggered.connect(self.run)
+        self.iface.addToolBarIcon(self.action)
+
+    def unload(self):
+        self.iface.removeToolBarIcon(self.action)
+        del self.action
+
+    def run(self):
+        QMessageBox.information(None, 'Test', 'Do something useful here')
