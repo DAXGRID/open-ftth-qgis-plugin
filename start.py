a031e1f135af4687c60f5e8f92b66ea9f283ad1a
Rune Nielsen <runeanielsen@live.dk>
2020-08-10 20:43:07 +0200

    adds group actions

--- start.py
+++ start.py
@@ -8,22 +8,34 @@ from .quick_edit_map_tool import QuickEditMapTool
 class Start:
     def __init__(self, iface):
         self.iface = iface
+        self.map_canvas = self.iface.mapCanvas()
+        self.map_tool = QuickEditMapTool(self.map_canvas)
 
     def initGui(self):
         self.setupActions()
-        #self.map_canvas = self.iface.mapCanvas()
-        #self.map_tool = QuickEditMapTool(self.map_canvas)
 
     def setupActions(self):
+        self.actions = []
+
         icon_path = ':/plugins/open_ftth/icon.png'
         self.action = QAction(QtGui.QIcon(icon_path), "Open Ftth", self.iface.mainWindow())
         self.action.setCheckable(True)
         self.action.triggered.connect(self.run)
+
+        self.action_group = QtWidgets.QActionGroup(self.iface.mainWindow())
+        self.action_group.setExclusive(True)
+        self.action_group.addAction(self.action)
+
+        self.actions.append(self.action)
+        self.iface.addPluginToMenu("&Open Ftth", self.action)
         self.iface.addToolBarIcon(self.action)
 
     def unload(self):
-        self.iface.removeToolBarIcon(self.action)
-        del self.action
+        for action in self.actions:
+            self.iface.removePluginMenu(action)
+            self.iface.removeToolBarIcon(action)
+
+        self.action.setChecked(False)
 
     def run(self):
         self.route_segment_layer = QgsProject.instance().mapLayersByName('route_segment')[0]
