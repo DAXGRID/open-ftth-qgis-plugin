f4018c1d4537faaa566178d6ba08f551c2de96e1
Rune Nielsen <runeanielsen@live.dk>
2020-08-11 15:39:58 +0200

    handle multiple activations

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
 from PyQt5 import QtWidgets, QtGui;
 from PyQt5.QtWidgets import QAction, QMessageBox
 from qgis.core import QgsVectorLayer, QgsProject
-import os.path
 from .resources import *
 from .quick_edit_map_tool import QuickEditMapTool
 
@@ -10,6 +9,7 @@ class Start:
         self.iface = iface
         self.map_canvas = self.iface.mapCanvas()
         self.map_tool = QuickEditMapTool(self.map_canvas)
+        self.layer_modified_enabled = False;
 
     def initGui(self):
         self.setupActions()
@@ -23,7 +23,6 @@ class Start:
         self.action.triggered.connect(self.run)
 
         self.action_group = QtWidgets.QActionGroup(self.iface.mainWindow())
-        self.action_group.setExclusive(True)
         self.action_group.addAction(self.action)
 
         self.actions.append(self.action)
@@ -32,17 +31,20 @@ class Start:
 
     def unload(self):
         for action in self.actions:
-            self.iface.removePluginMenu(action)
             self.iface.removeToolBarIcon(action)
 
         self.action.setChecked(False)
+        self.layer_modified_enabled = False
 
     def run(self):
-        self.route_segment_layer = QgsProject.instance().mapLayersByName('route_segment')[0]
-        self.route_segment_layer.layerModified.connect(self.saveActiveLayerEdits)
+        if self.layer_modified_enabled is False:
+            self.route_segment_layer = QgsProject.instance().mapLayersByName('route_segment')[0]
+            self.route_segment_layer.layerModified.connect(self.saveActiveLayerEdits)
 
-        self.route_node_layer = QgsProject.instance().mapLayersByName('route_node')[0]
-        self.route_node_layer.layerModified.connect(self.saveActiveLayerEdits)
+            self.route_node_layer = QgsProject.instance().mapLayersByName('route_node')[0]
+            self.route_node_layer.layerModified.connect(self.saveActiveLayerEdits)
+
+            self.layer_modified_enabled = True
 
     def saveActiveLayerEdits(self):
         self.iface.actionSaveActiveLayerEdits().trigger()
