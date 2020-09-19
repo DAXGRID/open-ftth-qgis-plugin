e6353f669718d6b1a1abace414d6bda7f0535a37
Rune Nielsen <runeanielsen@live.dk>
2020-09-20 00:57:47 +0200

    removes on open

--- src/listen_websockets.py
+++ src/listen_websockets.py
@@ -12,22 +12,10 @@ class ListenWebsocket(QtCore.QThread):
         self.ws = websocket.WebSocketApp("ws://localhost:5000/ws",
                                 on_message = lambda ws,msg: self.on_message(ws, msg),
                                 on_error = lambda ws,msg: self.on_error(ws, msg),
-                                on_close = lambda ws: self.on_close(ws),
                                 on_open = lambda ws: self.on_open(ws)) 
 
     def run(self):
         self.ws.run_forever()
-
-    def on_open(self, ws):
-        def run(*args):
-            for i in range(10000):
-                time.sleep(0.1)
-                ws.send("Hello %d" % i)
-            time.sleep(0.1)
-            ws.close()
-            print("thread terminating...")
-        thread.start_new_thread(run, ())
-
 
     def on_message(self, ws, message):
         print(message)
