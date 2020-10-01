aa464074155da0e2e3b6b883f6592e99cc2bae26
Rune Nielsen <runeanielsen@live.dk>
2020-10-02 00:58:51 +0200

    adds handling for getting selected features

--- src/event_handler.py
+++ src/event_handler.py
@@ -2,6 +2,7 @@ import json
 import base64
 from types import SimpleNamespace
 from .events.geographical_area_updated_handler import GeographicalAreaUpdatedHandler
+from .events.get_selected_features_handler import GetSelectedFeaturesHandler
 
 class EventHandler:
     def __init__(self, iface):
@@ -13,6 +14,8 @@ class EventHandler:
 
         if deserializedObject.eventType == "ObjectsWithinGeographicalAreaUpdated":
             GeographicalAreaUpdatedHandler(self.iface).handle();
+        elif deserializedObject.eventType == "GetSelectedFeatures":
+            GetSelectedFeaturesHandler(self.iface).handle();
             
     def deserialize(self, jsonMessage):
         return json.loads(jsonMessage, object_hook=lambda d: SimpleNamespace(**d))
