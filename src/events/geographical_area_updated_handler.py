bd0a4a9590d5eec719aba4675999fdc29423c64b
Rune Nielsen <runenielsen@runbox.com>
2021-12-09 15:01:31 +0100

    now only repaints in zoom levels under 1500 scale (#61)

--- src/events/geographical_area_updated_handler.py
+++ src/events/geographical_area_updated_handler.py
@@ -9,6 +9,9 @@ class GeographicalAreaUpdatedHandler:
         self.applicationSettings = ApplicationSettings()
 
     def handle(self, message):
+        if not self.zoom_close_enough():
+            return
+
         minX = message.envelope.minX
         maxX = message.envelope.maxX
         minY = message.envelope.minY
@@ -19,3 +22,6 @@ class GeographicalAreaUpdatedHandler:
         if self.iface.mapCanvas().extent().contains(extent):
             QgsProject.instance().mapLayersByName(self.applicationSettings.get_layers_route_segment_name())[0].triggerRepaint()
             QgsProject.instance().mapLayersByName(self.applicationSettings.get_layers_route_node_name())[0].triggerRepaint()
+
+    def zoom_close_enough(self):
+       return self.iface.mapCanvas().scale() < 1500
