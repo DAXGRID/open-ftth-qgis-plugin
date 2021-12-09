2a8f1e6fcf3f2de495cf26f27df747ad23284ed8
Rune Nielsen <runenielsen@runbox.com>
2021-12-09 17:27:56 +0100

    clear all snap locators on change (#62)

--- src/events/geographical_area_updated_handler.py
+++ src/events/geographical_area_updated_handler.py
@@ -18,10 +18,14 @@ class GeographicalAreaUpdatedHandler:
         maxY = message.envelope.maxY
 
         extent = QgsRectangle(minX, minY, maxX, maxY)
-
-        if self.iface.mapCanvas().extent().contains(extent):
+        if self.iface.mapCanvas().extent().intersects(extent):
             QgsProject.instance().mapLayersByName(self.applicationSettings.get_layers_route_segment_name())[0].triggerRepaint()
             QgsProject.instance().mapLayersByName(self.applicationSettings.get_layers_route_node_name())[0].triggerRepaint()
+
+            # We only want to clear locators when the geoms changes.
+            # This is because the clear locators are an expensive operation.
+            if message.category == "RouteNetworkUpdated":
+                self.iface.mapCanvas().snappingUtils().clearAllLocators()
 
     def zoom_close_enough(self):
        return self.iface.mapCanvas().scale() < 1500
