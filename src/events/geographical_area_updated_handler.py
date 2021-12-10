2f7df35a3c53e32f1c539ac0dc9d180e689848b0
Rune Nielsen <runenielsen@runbox.com>
2021-12-10 12:45:12 +0100

    handles invalid snap cache

--- src/events/geographical_area_updated_handler.py
+++ src/events/geographical_area_updated_handler.py
@@ -7,6 +7,7 @@ class GeographicalAreaUpdatedHandler:
     def __init__(self, iface):
         self.iface = iface
         self.applicationSettings = ApplicationSettings()
+        self.iface.mapCanvas().snappingUtils().setIndexingStrategy(self.iface.mapCanvas().snappingUtils().IndexingStrategy.IndexExtent)
 
     def handle(self, message):
         if not self.zoom_close_enough():
@@ -17,15 +18,20 @@ class GeographicalAreaUpdatedHandler:
         minY = message.envelope.minY
         maxY = message.envelope.maxY
 
+        segmentLayer = QgsProject.instance().mapLayersByName(self.applicationSettings.get_layers_route_segment_name())[0]
+        nodeLayer = QgsProject.instance().mapLayersByName(self.applicationSettings.get_layers_route_node_name())[0]
+
         extent = QgsRectangle(minX, minY, maxX, maxY)
         if self.iface.mapCanvas().extent().intersects(extent):
-            QgsProject.instance().mapLayersByName(self.applicationSettings.get_layers_route_segment_name())[0].triggerRepaint()
-            QgsProject.instance().mapLayersByName(self.applicationSettings.get_layers_route_node_name())[0].triggerRepaint()
-
             # We only want to clear locators when the geoms changes.
             # This is because the clear locators are an expensive operation.
             if message.category == "RouteNetworkUpdated":
+                segmentLayer.reload()
+                nodeLayer.reload()
                 self.iface.mapCanvas().snappingUtils().clearAllLocators()
+
+            segmentLayer.triggerRepaint()
+            nodeLayer.triggerRepaint()
 
     def zoom_close_enough(self):
        return self.iface.mapCanvas().scale() < 1500
